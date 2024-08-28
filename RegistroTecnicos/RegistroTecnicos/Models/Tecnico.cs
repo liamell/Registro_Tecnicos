5de5a2388089de27bfc1100570b47c3e0b5f79cd
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2024-08-28 13:47:31 -0400

    Hice el contexto

---
+++ RegistroTecnicos/RegistroTecnicos/Models/Tecnico.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnico
+{
+    [Key]
+    public int EstudianteId { get; set; }
+    public string? Nombre { get; set; }
+    public int SueldoHora { get; set; }
+}
